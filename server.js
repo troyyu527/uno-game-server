e10027a839c53d6ea73d801ee97dc8b55b5c77fe
Glitch (glitch-blank-node) <none>
2023-05-25 21:53:45 +0000

    🐞🕵 Checkpoint ./package.json:58487195/44 ./server.js:58487195/373

--- server.js
+++ server.js
@@ -32,17 +32,7 @@ app.use("/api/user",authRoute)
 //Use passport to protect all routes after /api
 app.use("/api",passport.authenticate("jwt",{session:false}),itemRoute)
 
-// app.listen(8080,()=>{
-//   console.log("Server is running on port 8080")
-// })
 // Run the server and report out to the logs
-app.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
+app.listen(8080,()=>{
+  console.log("Server is running on port 8080")
+})
